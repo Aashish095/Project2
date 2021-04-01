03c8a7bef684dcc8a90b2c9d6575e4d85574abfe
Aashish095 <aashujain095@gmail.com>
2021-04-02 01:05:47 +0530

    changes in signup pages

--- apptwo/views.py
+++ apptwo/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from apptwo.models import user
+# from apptwo.models import user
+from apptwo.form import newuser
 # Create your views here.
 
 def index(request):
     return render(request,'apptwo/index.html')
 
 def users(request):
-    user_list = user.objects.order_by('first_name')
-    user_dict = {'users' : user_list}
-    return render(request,'apptwo/user.html',context=user_dict)
+    form = newuser()
+
+    if request.method  == "POST":
+        form = newuser(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FORM INVALID")
+    return render(request,'apptwo/user.html',{'form':form})
+    
